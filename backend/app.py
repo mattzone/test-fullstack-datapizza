277c96798f1e2897badd3040a3d5b4237cec8aae
Matteo Fasano <matteo.fasano01@gmail.com>
2025-03-04 23:28:30 +0100

    add documents and fix cors

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,27 @@
 from flask import Flask, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, support_credentials=True)
 
 @app.route('/documents')
 def get_documents():
     return {
         'documents': [
-            {'title': 'Doc 1', 'content': 'Questo è l\'inizio di un paragrafo...'},
-            {'title': 'Doc 2', 'content': 'Questo è l\'inizio di un altro paragrafo...'}
-        ]
+  {
+    "title": "Doc 1",
+    "content": "In un mondo pieno di infinite possibilità, le opportunità di esplorazione sono altrettanto illimitate. Continuando la nostra avventura, scopriremo nuovi orizzonti e supereremo gli ostacoli che incontreremo lungo il cammino. Non ci fermeremo mai, perché la nostra sete di conoscenza ci spinge sempre avanti."
+  },
+  {
+    "title": "Doc 2",
+    "content": "Immagina un futuro in cui la tecnologia e la creatività lavorano insieme in perfetta armonia. In questo mondo, le idee innovative prendono vita con facilità e le barriere che una volta ci trattenevano cadono come foglie in autunno. Il potere di trasformare i nostri sogni in realtà è alla nostra portata, basta avere il coraggio di afferrarlo."
+  },
+  {
+    "title": "Doc 3",
+    "content": "La bellezza della natura ci circonda ovunque andiamo, offrendoci un rifugio tranquillo dal trambusto della vita quotidiana. Ogni albero, fiore e ruscello racconta una storia di crescita e resilienza, insegnandoci lezioni preziose su come affrontare le sfide con grazia e determinazione. Abbracciando questa saggezza, possiamo trovare la pace interiore e la forza necessaria per prosperare."
+  }
+]
+
     }
 
 @app.route('/generate',  methods = ['POST'])
