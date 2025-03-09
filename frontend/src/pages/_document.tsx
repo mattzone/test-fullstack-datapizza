e9bdd1755446f822c922414cb0c3ba76fee4c893
Matteo Fasano <matteo.fasano01@gmail.com>
2025-03-09 22:18:50 +0300

    change title

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -2,8 +2,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="it">
       <Head />
+      <title>Test FullStack Developer Fasano</title>
       <body>
         <Main />
         <NextScript />
